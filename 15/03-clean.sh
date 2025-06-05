6f938b01b6c48c647ba2dcbb0b26515cabcb7ab2
AidanWarner97 <aidan@awarner.co.uk>
2025-06-05 10:18:07 +0200

    Update CCACHE_MAXSIZE to 300G

--- 15/03-clean.sh
+++ 15/03-clean.sh
@@ -4,7 +4,7 @@ device=$1
 
 target=$(tail -n 1 vendor/lineage/vars/aosp_target_release | cut -d "=" -f 2)
 
-export CCACHE_MAXSIZE=100G
+export CCACHE_MAXSIZE=300G
 
 source build/envsetup.sh &&
 lunch lineage_$device-$target-userdebug &&
--- 15/04-build.sh
+++ 15/04-build.sh
@@ -5,7 +5,7 @@ device=$1
 target=$(tail -n 1 vendor/lineage/vars/aosp_target_release | cut -d "=" -f 2)
 
 export EVO_BUILD_TYPE=Official
-export CCACHE_MAXSIZE=100G
+export CCACHE_MAXSIZE=300G
 
 source build/envsetup.sh &&
 lunch lineage_$device-$target-user &&
