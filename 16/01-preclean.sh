9bc8e50298ba8fd8afb31b484cc845091c911033
objectfigure <hlcj1234567@gmail.com>
2025-06-13 22:41:56 +0100

    Jenkins: Added Experimental Android 16 support

---
+++ 16/01-preclean.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if  [ -f ".repo/local_manifests/roomservice.xml" ]; then
+    rm .repo/local_manifests/roomservice.xml
+fi
+
+devices="asus xiaomi realme motorola miromax wingtech oneplus lenovo samsung google"
+for device in $devices; do
+    if [[ -d device/$device ]]; then rm -rf device/$device; fi
+    if [[ -d kernel/$device ]]; then rm -rf kernel/$device; fi
+    if [[ -d vendor/$device ]]; then rm -rf vendor/$device; fi
+done
+
+if [[ -d "vendor/google" ]]; then 
+    rm -rf vendor/google
+fi
+
+if [ -d "evolution/OTA" ]; then
+    rm -rf evolution/OTA
+fi
+
+cd vendor/gms
+git reset HEAD --hard
+cd /evo-10
---
+++ 16/03-clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+device=$1
+
+target=$(tail -n 1 vendor/lineage/vars/aosp_target_release | cut -d "=" -f 2)
+
+export CCACHE_MAXSIZE=300G
+
+source build/envsetup.sh &&
+lunch lineage_$device-$target-userdebug &&
+m clean
